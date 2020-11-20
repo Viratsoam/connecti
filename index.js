08605005607f3f7818ea7edb8e9ffd4468fa53bc
Viratsoam <viratsoam1105>
2020-11-20 11:38:50 +0530

    setup the view engine

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 
+//use routes
+app.use("/", require("./routes/index"));
+
+// setup the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error in running the server:${err}`);
