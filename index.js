8866351c4b5033f4c9e285e254fbe36ff47e6328
Viratsoam <viratsoam1105>
2020-12-30 14:04:45 +0530

    added static files

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
+const expressLayouts = require("express-ejs-layouts");
 
+// use static file
+app.use(express.static("./assets"));
+// use layout / set Style and script
+app.use(expressLayouts);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 // setup the view engine
 app.set("views", "./views");
 app.set("view engine", "ejs");
